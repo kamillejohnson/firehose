4992867d573d1c8a784d2665893c59f841953c2d
kamille johnson <kamille@kamilles-mbp.risd.edu>
2014-02-09 12:43:46 -0500

    fixing typos...

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    confix.storage = :fog
+    config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
